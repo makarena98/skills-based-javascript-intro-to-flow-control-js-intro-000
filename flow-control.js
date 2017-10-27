8022b65e45f748a826ff8771ace1a04af4e3bc33
Karen Allen <starting_today@yahoo.com>
2017-10-27 16:59:09 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -22,34 +22,36 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-if (age>=13 && age <=19){
-  return "You are a teenager"}
+  if (age>=13 && age <=19){
+   return "You are a teenager"
+  }
   return "You are not a teenager"
 }
 
 function switchAge(age) {
-switch(age){
-  case age === 13:
-  return "You are a teenager";
-  break;
-  case age === 14:
-  return "You are a teenager";
-  break;
-  case age === 15:
-  return "You are a teenager";
-  break;
-  case age === 16:
-  return "You are a teenager";
-  break;
-  case age === 17:
-  return "You are a teenager";
-  break;
-  case age === 18:
-  return "You are a teenager";
-  break;
-  case age === 19:
-  return "You are a teenager";
-  break;
-  default:
-  return "You have an age"}
+  switch(age){
+    case 13:
+    return "You are a teenager";
+    break;
+    case 14:
+    return "You are a teenager";
+    break;
+    case 15:
+    return "You are a teenager";
+    break;
+    case 16:
+    return "You are a teenager";
+    break;
+    case 17:
+    return "You are a teenager";
+    break;
+    case 18:
+    return "You are a teenager";
+    break;
+    case 19:
+    return "You are a teenager";
+    break;
+    default:
+    return "You have an age"
+  }
 }
